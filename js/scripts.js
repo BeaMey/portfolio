042aae483a970b7a76c28984febc691998af2981
beamey <beatrizmeythalerg@gmail.com>
2023-10-28 13:22:19 +0200

    Fixing JS
    This commit fixes JS for testimonials.

--- js/scripts.js
+++ js/scripts.js
@@ -22,27 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 });
 
-// Table of contents
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-
 // Testimonials slider
-
 let testSlide = document.querySelectorAll('.testItem');
 let dots = document.querySelectorAll('.dot');
 var counter = 0;
@@ -138,3 +118,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// Table of contents
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+  
