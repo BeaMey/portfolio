c197114ba03c070bd8eaba1d13779dcb99cea9e7
beamey <beatrizmeythalerg@gmail.com>
2024-11-21 17:01:14 +0100

    german pro

--- js/scripts.js
+++ js/scripts.js
@@ -116,4 +116,17 @@ function openCity(evt, cityName) {
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
   
-// photo slider// 
+// German Pro// 
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent-germanpro");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
